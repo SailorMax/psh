247f8c22f3fecfd31eb6e7970d5fda924b11389f
Maxim <sailormax@live.com>
2023-03-29 23:53:13 +0300

    readme + comments + clean code

--- psh.bat
+++ psh.bat
@@ -1,5 +1,5 @@
 @echo off
-chcp 65001
+chcp 65001 > nul
 
 cd /D "%~dp0"
 powershell -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned; .\session_chooser.ps1" %*
--- session_chooser.ps1
+++ session_chooser.ps1
@@ -1,43 +1,51 @@
-# Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+# init
+$RegistryPath = "Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions\"
 
 # get all sessions
-$Sessions = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions\* |
+$Sessions = Get-Item -Path "$($RegistryPath)*" |
 			Select-Object -ExpandProperty Name |
 			Split-Path -leaf |
 			Sort-Object
 
 # filter list
-$mask=$args[0]
+$mask = $args[0]
 if ($mask.Length -gt 0) {
 	$Sessions = $Sessions | Where-Object {$_ -match $mask}
 }
 
 # output list
-$counter=1;
+$counter = 1;
 Write-Host "0. putty"
 $Sessions | ForEach-Object {"$($counter). $($_)"; $counter++}
 Write-Host "---"
+# ask to choose
 [ValidateScript({$_ -ge 0 -and $_ -le $Sessions.Length})]
 [int]$Number = Read-Host "Choose session by number"
+if (!$?) {
+	exit
+}
 
-# exec
-if ($?) {
-	if ($Number -eq 0) {
-		putty
-	} else {
-		$HostPortUser = Get-ItemPropertyValue -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions\$($Sessions[$Number-1]) -Name HostName, PortNumber, UserName, UserNameFromEnvironment
-		Write-Host "connecting to $($Sessions[$Number-1]) ($($HostPortUser[0]):$($HostPortUser[1]))..."
+# execute
+if ($Number -eq 0) {
+	Write-Host "Start putty"
+	putty
+} else {
+	# get connection parameters
+	$HostPortUser = Get-ItemPropertyValue -Path "$($RegistryPath)$($Sessions[$Number-1])" -Name HostName, PortNumber, UserName, UserNameFromEnvironment
+	Write-Host "connecting to $($Sessions[$Number-1]) ($($HostPortUser[0]):$($HostPortUser[1]))..."
 
-		$UserName=""
-		if ($HostPortUser[3] -eq 0) {
-			$UserName=$HostPortUser[2]
-			if ($UserName.Length -eq 0) {
-				[string]$UserName = Read-Host "login as"
-			}
-			$UserName="$UserName@"
+	# get username
+	$UserName=""
+	if ($HostPortUser[3] -eq 0) {
+		$UserName=$HostPortUser[2]
+		if ($UserName.Length -eq 0) {
+			# ask username
+			[string]$UserName = Read-Host "login as"
 		}
-
-		$UserHost = "$UserName$($HostPortUser[0])"
-		ssh $UserHost -p $HostPortUser[1]
+		$UserName="$UserName@"
 	}
+
+	# start session
+	$UserHost = "$($UserName)$($HostPortUser[0])"
+	ssh -p $HostPortUser[1] $UserHost
 }
